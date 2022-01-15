2e5d9a3f2d39474a6cb1bf13076de8340401e7b4
Ivan de Andrade Santana <ivanandradesantana@gmail.com>
2022-01-15 01:08:33 -0300

    Security config

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -1,4 +1,4 @@
-import './styles.css';
+import "./styles.css";
 import { Link} from  "react-router-dom";
 
 const movie = {
@@ -35,7 +35,7 @@ function Form()
                             <button type="submit" className="btn btn-primary dsmovie-btn">Salvar</button>
                         </div>
                     </form >
-                    <Link to='/'>
+                    <Link to="/">
                         <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
                     </Link>                    
                 </div >
